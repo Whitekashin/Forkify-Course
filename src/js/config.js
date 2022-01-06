7dac06a5fc1b8f340b0e2be98e793f9e4082111c
Whitekashin <patrick.reitbauer@web.de>
2022-01-06 10:46:07 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '379d9515-f217-4336-b26a-b56aa6c6693d';
+export const MODAL_CLOSE_SEC = 2.5;
